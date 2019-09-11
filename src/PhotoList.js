e0fe487aaeae767b3d18b6109c9c073cfb783ee0
CScori <amilanotte1366@gmail.com>
2019-09-11 15:46:02 -0500

    hit mvp early -> button allowing date change

--- src/PhotoList.js
+++ src/PhotoList.js
@@ -4,10 +4,11 @@ import PhotoCard from "./PhotoCard";
 
 const PhotoList = () => {
     const [photo, setPhoto] = useState([]);
+    const [date, setDate] = useState('2012-03-14')
 
     useEffect(() => {
         axios
-        .get(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY`)
+        .get(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=${date}`)
         .then(res => {
             const item = res.data
             console.log('return from api', item)
@@ -16,7 +17,7 @@ const PhotoList = () => {
         .catch(err => {
             console.log('api err or timeout', err)
         })
-    }, [])
+    }, [date])
     return (
         <div className='map-list'>
             {photo.map(item => {
@@ -24,8 +25,12 @@ const PhotoList = () => {
                 alt={item.title}
                 src={item.url}
                 description={item.explanation}
+                date={date}
                 />
             })}
+            
+                <button onClick={() setDate(2011-12-11)}>Change The Date</button>
+
         </div>
     )
 }
