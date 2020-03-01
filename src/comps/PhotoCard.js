5807fd5561b69aa23e400ea870fe9898213b7227
CScori <49799016+CScori@users.noreply.github.com>
2020-02-29 19:29:04 -0800

    adds width restriction

--- src/comps/PhotoCard.js
+++ src/comps/PhotoCard.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import {
   Card, CardImg, CardText, CardBody,
-  CardTitle
+  CardTitle, Col
 } from 'reactstrap';
 
 const PhotoCard = props => {
 
     return (
+      <Col xs='auto'>
             <Card body inverse style={{ backgroundColor: '#333', borderColor: '#333' }} width='70%' key={props.id}> 
               <CardImg top width="25%" src={props.img} alt={props.title} />
               <CardBody>
@@ -14,6 +15,7 @@ const PhotoCard = props => {
                 <CardText>Image Description: {props.explanation}</CardText>
               </CardBody>
             </Card>
+            </Col>
         );
       };
 
